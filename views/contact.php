ff687a73a3c0c604a8b0d4bbab39817c8ded77e6
KissRoli96 <kissrolandkristof@gmail.com>
2021-09-22 10:28:33 +0200

    added the view files

---
+++ views/contact.php
@@ -0,0 +1,19 @@
+<h1>
+    Contact Us
+</h1>
+
+<form action="" method="post">
+    <div class="mb-3">
+        <label>Subject</label>
+        <input type="text" class="form-control" name="subject" >
+    </div>
+    <div class="mb-3">
+        <label>Email address</label>
+        <input type="email" class="form-control" name="email" >
+    </div>
+    <div class="mb-3">
+        <label>Body</label>
+        <textarea class="form-control" name="body" ></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ views/home.php
@@ -0,0 +1,7 @@
+<h1>
+    Home
+</h1>
+<h3>
+    <?= "Welcome to Home, $name" ?>
+</h3>
+
