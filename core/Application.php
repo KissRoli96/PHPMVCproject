dcc83675d70030385719dea62d54abe9f73d82cb
KissRoli96 <kissrolandkristof@gmail.com>
2021-09-22 10:15:13 +0200

    added some public members: root_dir,response,app,controller,controller getter and setter

--- core/Application.php
+++ core/Application.php
@@ -9,16 +9,41 @@ namespace app\core;
 
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
-    public function __construct()
+    public Response $response;
+    public static Application $app;
+    public Controller $controller;
+
+    /**
+     * @return Controller
+     */
+    public function getController(): Controller
     {
+        return $this->controller;
+    }
+
+    /**
+     * @param Controller $controller
+     */
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
+    }
+
+
+    public function __construct($rootPath)
+    {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response= new Response();
+        $this->router = new Router($this->request,$this->response);
     }
 
     public function run()
     {
-        $this->router->resolve();
+      echo  $this->router->resolve();
     }
 }
