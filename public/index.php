ee64dc3b410518affb5136253c14503e8301d8d3
KissRoli96 <kissrolandkristof@gmail.com>
2021-09-22 10:05:06 +0200

    added the controllers and routes to index file

--- public/index.php
+++ public/index.php
@@ -1,18 +1,21 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\controllers\SiteController;
 use app\core\Application;
+use app\controllers\AuthController;
 
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
 
-$app->router->get('/', function (){
-    return "Hello world";
-});
+$app->router->get('/', [SiteController::class, 'home']);
 
-$app->router->get('/contact', function (){
-    return "Contact";
-});
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, 'handleContact']);
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 $app->run();
-
