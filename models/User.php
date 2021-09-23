5b15cf529305586d063428754cbac7546aeb041a
KissRoli96 <kissrolandkristof@gmail.com>
2021-09-23 15:44:48 +0200

    restrict registration with same email

--- models/User.php
+++ models/User.php
@@ -26,7 +26,7 @@ class User extends DbModel
 
     public function save()
     {
-        $this->status = self::STATUS_DELETED;
+        $this->status = self::STATUS_INACTIVE;
         $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return parent::save();
 
@@ -38,7 +38,9 @@ class User extends DbModel
         return [
             'firstname' => [self::RULE_REQUIRED],
             'lastname' => [self::RULE_REQUIRED],
-            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
+                self::RULE_UNIQUE, 'class' => self::class,
+            ]],
             'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
             'confirmPassword' => [self::RULE_REQUIRED,[self::RULE_MATCH, 'match' => 'password']],
         ];
